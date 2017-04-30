553c27ffccb697154b2c036aa2153beec23a450e
ciaranRoche <ciaranroche@hotmail.co.uk>
2017-04-30 21:58:56 +0100

    back to basic, outputting dynamic data

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -5,9 +5,14 @@ import React from "react";
 
 export class Home extends React.Component{
     render(){
+        let content = "";
+        if(true){
+            content = <p>Hello World</p>
+        }
         return(
             <div>
                 <p>In a new Component!</p>
+                {content}
             </div>
         );
     }

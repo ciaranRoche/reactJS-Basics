759d99b94c5f2346df54a9ec34fe49d5a07f5551
ciaranRoche <ciaranroche@hotmail.co.uk>
2017-05-01 11:37:24 +0100

    Passing data with props

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 #ReactJS Basics
 My first attempt at working with ReactJS. Going from setting up workspace with webpack, my first component right up to navigation and parameters. This is a side project I will be working on over my forth semester in college. 
+### Dependencies
+* react
+* react dom
+
+### Dev Dependencies
+* babel-core
+* babel-loader
+* babel-preset-es2015
+* babel-preset-react
+* babel-preset-stage-2
+* webpack
+* webpack-dev-server
--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -5,15 +5,30 @@ import React from "react";
 
 export class Home extends React.Component{
     render(){
-        let content = "";
-        if(true){
-            content = <p>Hello World</p>
-        }
+        var random = "Hello there my old friend"
+        console.log(this.props);
         return(
             <div>
                 <p>In a new Component!</p>
-                {content}
+                <p>{random}</p>
+                <p>Your name is {this.props.name} and your age is {this.props.age}</p>
+                <p>User Object => Name : {this.props.user.name}</p>
+                <div>
+                    <h4>Hobbies</h4>
+                    <ul>
+                        {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
+                    </ul>
+                </div>
+                <hr/>
+                {this.props.children}
             </div>
         );
     }
 }
+
+Home.propTypes = {
+    name : React.PropTypes.string,
+    age : React.PropTypes.number,
+    user : React.PropTypes.object,
+    childen : React.PropTypes.element.isRequired
+};

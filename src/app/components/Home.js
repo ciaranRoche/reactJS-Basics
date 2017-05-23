7d4bb227363110fcd4938eadb312cf41de9c45db
ciaranRoche <ciaranroche@hotmail.co.uk>
2017-05-23 14:17:25 +0100

    component lifecycle

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -16,6 +16,36 @@ export class Home extends React.Component {
                 status: 1
             });
         },3000);
+        console.log("Constructor");
+    }
+
+    componentWillMount(){
+        console.log("Component will mount");
+    }
+
+    componentDidMount(){
+        console.log("Component did mount");
+    }
+
+    componentWillReceiveProps(nextProps){
+        console.log("Component will receive props", nextProps);
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+        console.log("Should Component update", nextProps, nextState);
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState){
+        console.log("Component will update", nextProps, nextState);
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        console.log("Component did update", prevProps, prevState);
+    }
+
+    componentWillUnmount(){
+        console.log("Component will unmount");
     }
 
     onMakeOlder() {
